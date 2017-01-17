c306c1736fab3efa2f75b47471c304c047d0779e
Michaël BERTONI <michael@thynkio.com>
2017-01-17 19:12:40 +0100

    Création du .war

--- src/main/java/com/currencyconverter/api/CurrencyConverterSpringApiApplication.java
+++ src/main/java/com/currencyconverter/api/CurrencyConverterSpringApiApplication.java
@@ -2,9 +2,16 @@ package com.currencyconverter.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CurrencyConverterSpringApiApplication {
+public class CurrencyConverterSpringApiApplication extends SpringBootServletInitializer {
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(CurrencyConverterSpringApiApplication.class);
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(CurrencyConverterSpringApiApplication.class, args);
